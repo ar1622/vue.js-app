15fd3ea1b62ee313f3c421640fe6bf3c14bad37c
ar1622 <ar1622@live.mdx.ac.uk>
2024-01-28 17:23:22 +0600

    update lesson component

--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
 // main.js
 
-import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import "bootstrap"; // Import Bootstrap JavaScript
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 
 import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
+import Cart from "./pages/Cart.vue";
+import CreateNewLesson from "./pages/CreateNewLesson.vue";
 import Lesson from "./pages/Lesson.vue";
 import Order from "./pages/Order.vue";
-import CreateNewLesson from "./pages/CreateNewLesson.vue";
-import Cart from "./pages/Cart.vue";
+import SearchItem from './pages/SearchItem.vue';
 import store from "./store";
-import SearchItem from './pages/SearchItem.vue'
 
 // Create the router
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: "/", component: Lesson },
+        { path: "/vue.js-app", component: Lesson },
         { path: "/lesson", component: Lesson },
         { path: "/order", component: Order },
         { path: "/create-new-lesson", component: CreateNewLesson },
