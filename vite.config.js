73c4b94aaad7456d76a5d9811e667024b9faeb67
ar1622 <ar1622@live.mdx.ac.uk>
2024-01-28 17:20:45 +0600

    update vit config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: "/",
+    base: "/vue.js-app",
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
